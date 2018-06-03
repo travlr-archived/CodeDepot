83c1d51c04e9af87e65adea7741eaabc4a6779bd
Peter Alexander <peter.alexander@prodatalab.com>
2018-06-03 08:46:14 -0600

    added mindmap dir and codedepot mindmap

---
+++ mindmaps/CodeDepot.mm
@@ -0,0 +1,124 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="CodeDepot" FOLDED="false" ID="ID_686825740" CREATED="1528034566527" MODIFIED="1528035089945" STYLE="oval">
+<font SIZE="20"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Governence" POSITION="right" ID="ID_81915571" CREATED="1528034618977" MODIFIED="1528035966753">
+<edge COLOR="#ff0000"/>
+<font SIZE="20"/>
+</node>
+<node TEXT="MarketPlace" POSITION="right" ID="ID_1669500385" CREATED="1528034629233" MODIFIED="1528036062999">
+<edge COLOR="#0000ff"/>
+<font SIZE="20"/>
+<node TEXT="Software Products" ID="ID_1981048938" CREATED="1528034772876" MODIFIED="1528035089946">
+<font SIZE="20"/>
+</node>
+<node TEXT="Help Desk" ID="ID_1256321528" CREATED="1528034795012" MODIFIED="1528034942554">
+<font SIZE="20"/>
+</node>
+<node TEXT="Tuteledge" ID="ID_725791014" CREATED="1528034980329" MODIFIED="1528035089946">
+<font SIZE="20"/>
+</node>
+<node TEXT="Publishers" ID="ID_831864491" CREATED="1528035181474" MODIFIED="1528036095954">
+<font SIZE="20"/>
+<node TEXT="Books" ID="ID_591700947" CREATED="1528036542559" MODIFIED="1528036568359">
+<font SIZE="20"/>
+</node>
+<node TEXT="Courses" ID="ID_1644127979" CREATED="1528036568845" MODIFIED="1528036589715">
+<font SIZE="20"/>
+</node>
+<node TEXT="Audio" LOCALIZED_STYLE_REF="default" ID="ID_1226368623" CREATED="1528036590717" MODIFIED="1528036797432">
+<font SIZE="20"/>
+</node>
+<node TEXT="Video" ID="ID_1293377445" CREATED="1528036798091" MODIFIED="1528036839776">
+<font SIZE="20" BOLD="false"/>
+</node>
+<node TEXT="Documentation" ID="ID_1226899525" CREATED="1528036840387" MODIFIED="1528036855120">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Marketers" ID="ID_1516533050" CREATED="1528035224370" MODIFIED="1528036077088">
+<font SIZE="20"/>
+</node>
+<node TEXT="Freelancers / Consultants" ID="ID_246805898" CREATED="1528036101550" MODIFIED="1528036238475">
+<font SIZE="20"/>
+</node>
+<node TEXT="Job Board" ID="ID_1140383587" CREATED="1528036247436" MODIFIED="1528036494651">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Economics / Monetary Policy" POSITION="right" ID="ID_909693636" CREATED="1528034649894" MODIFIED="1528036007975">
+<edge COLOR="#00ff00"/>
+<font SIZE="20"/>
+</node>
+<node TEXT="Developer Tools" POSITION="right" ID="ID_1025685949" CREATED="1528034688984" MODIFIED="1528035089944">
+<edge COLOR="#ff00ff"/>
+<font SIZE="20"/>
+</node>
+<node TEXT="Distributed Computing, DB" POSITION="right" ID="ID_552828150" CREATED="1528034724807" MODIFIED="1528036943087">
+<edge COLOR="#00ffff"/>
+<font SIZE="20"/>
+</node>
+</node>
+</map>
